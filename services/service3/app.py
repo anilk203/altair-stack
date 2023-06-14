ac7981a34ef1319f808b3f9fb75841f5aceb15f2
anilk203 <anilk203@gmail.com>
2023-06-14 18:24:56 -0400

    landing and 3 services

---
+++ services/service3/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, render_template
+import datetime
+import os
+import csv
+import json
+import sys
+import uuid
+
+app = Flask(__name__)
+
+@app.route("/service3/")
+def home():
+    return render_template("index.html", utc_dt=datetime.datetime.utcnow())
+
+@app.route("/service3/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)    
+    app.run_server(debug=True)
